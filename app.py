090d80d67f33e83e49532e1ae716b6384288aca9
takeru-a <pypy20191022@gmail.com>
2022-08-11 16:24:51 +0900

    add:プログラムの追加

---
+++ app.py
@@ -0,0 +1,208 @@
+import queue
+import threading
+import mediapipe as mp
+import urllib.request
+from pathlib import Path
+from typing import List, NamedTuple
+from matchstick import Matchstick
+import av
+import cv2
+import numpy as np
+import pydub
+import streamlit as st
+from aiortc.contrib.media import MediaPlayer
+
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    webrtc_streamer,
+)
+
+path = Path(__file__).parent
+
+#WebRTCの設定
+RTC_CONFIGURATION = RTCConfiguration(
+    #Google提供のSTUNサーバー
+    {"Servers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    # Mozilla提供のSTUNサーバー
+    # {"Servers": [{"urls": ["stun.services.mozilla.com"]}]}
+)
+
+
+def main():
+    st.header("GAMES")
+
+    pages = {
+        "マッチ棒": matchstick,
+        "選択クイズ": app_video_filters,  
+        "もじぴったん": app_audio_filter,
+        "5sec_Late": app_media_constraints,  
+        "Customize UI texts": app_customize_ui_texts,
+    }
+    page_titles = pages.keys()
+
+    page_title = st.sidebar.selectbox(
+        "Choose the game",
+        page_titles,
+    )
+    st.subheader(page_title)
+
+    page_func = pages[page_title]
+    page_func()
+
+
+def matchstick():
+    st.title("QuizGame")
+    """
+    マッチ棒を一回だけ動かして正しい式に直してください
+    """
+    mp_hands = mp.solutions.hands #モデルの作成
+    game = Matchstick()
+    
+    def callback(frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
+        img = cv2.flip(img,1)
+        # img = game.combi(img)
+        hands = mp_hands.Hands(
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7,
+        max_num_hands = 1
+        )
+        results = hands.process(img)
+        if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    # img = game.move(img, hand_landmarks)
+                    game.hand(img, hand_landmarks)
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+    webrtc_streamer(
+        key="matchstick",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=callback,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
+
+def app_video_filters():
+    """Video transforms with OpenCV"""
+
+    _type = st.radio("Select transform type", ("noop", "cartoon", "edges", "rotate"))
+
+    def callback(frame: av.VideoFrame) -> av.VideoFrame:
+        img = frame.to_ndarray(format="bgr24")
+
+        if _type == "noop":
+            pass
+        elif _type == "cartoon":
+            # prepare color
+            img_color = cv2.pyrDown(cv2.pyrDown(img))
+            for _ in range(6):
+                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+            # prepare edges
+            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            img_edges = cv2.adaptiveThreshold(
+                cv2.medianBlur(img_edges, 7),
+                255,
+                cv2.ADAPTIVE_THRESH_MEAN_C,
+                cv2.THRESH_BINARY,
+                9,
+                2,
+            )
+            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+            # combine color and edges
+            img = cv2.bitwise_and(img_color, img_edges)
+        elif _type == "edges":
+            # perform edge detection
+            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+        elif _type == "rotate":
+            # rotate image
+            rows, cols, _ = img.shape
+            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+            img = cv2.warpAffine(img, M, (cols, rows))
+
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+    webrtc_streamer(
+        key="opencv-filter",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_frame_callback=callback,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
+
+    st.markdown(
+        "This demo is based on "
+        "https://github.com/aiortc/aiortc/blob/2362e6d1f0c730a0f8c387bbea76546775ad2fe8/examples/server/server.py#L34. "  # noqa: E501
+        "Many thanks to the project."
+    )
+
+def app_audio_filter():
+    gain = st.slider("Gain", -10.0, +20.0, 1.0, 0.05)
+
+    def process_audio(frame: av.AudioFrame) -> av.AudioFrame:
+        raw_samples = frame.to_ndarray()
+        sound = pydub.AudioSegment(
+            data=raw_samples.tobytes(),
+            sample_width=frame.format.bytes,
+            frame_rate=frame.sample_rate,
+            channels=len(frame.layout.channels),
+        )
+
+        sound = sound.apply_gain(gain)
+        channel_sounds = sound.split_to_mono()
+        channel_samples = [s.get_array_of_samples() for s in channel_sounds]
+        new_samples: np.ndarray = np.array(channel_samples).T
+        new_samples = new_samples.reshape(raw_samples.shape)
+
+        new_frame = av.AudioFrame.from_ndarray(new_samples, layout=frame.layout.name)
+        new_frame.sample_rate = frame.sample_rate
+        return new_frame
+
+    webrtc_streamer(
+        key="audio-filter",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        audio_frame_callback=process_audio,
+        async_processing=True,
+    )
+
+def app_media_constraints():
+    """A sample to configure MediaStreamConstraints object"""
+    frame_rate = 5
+    webrtc_streamer(
+        key="media-constraints",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        media_stream_constraints={
+            "video": {"frameRate": {"ideal": frame_rate}},
+        },
+        video_html_attrs={
+            "style": {"width": "50%", "margin": "0 auto", "border": "5px yellow solid"},
+            "controls": False,
+            "autoPlay": True,
+        },
+    )
+    st.write(f"The frame rate is set as {frame_rate}. Video style is changed.")
+
+def app_customize_ui_texts():
+    webrtc_streamer(
+        key="custom_ui_texts",
+        rtc_configuration=RTC_CONFIGURATION,
+        translations={
+            "start": "開始",
+            "stop": "停止",
+            "select_device": "デバイス選択",
+            "media_api_not_available": "Media APIが利用できない環境です",
+            "device_ask_permission": "メディアデバイスへのアクセスを許可してください",
+            "device_not_available": "メディアデバイスを利用できません",
+            "device_access_denied": "メディアデバイスへのアクセスが拒否されました",
+        },
+    )
+
+if __name__ == "__main__":
+    main()
---
+++ matchstick.py
@@ -0,0 +1,200 @@
+import cv2
+import numpy as np
+
+class Matchstick():
+    def __init__(self) -> None:
+        #マッチ棒の数
+        N = 22
+        #動かせないマッチ棒の指定
+        offflags = [3,7,10,15,17]
+        #表示するマッチ棒の選択
+        init_box = [1] * N
+        for n in offflags:
+            init_box[n] = 0
+        #掴んでいるかどうか
+        self.moving = False
+        #つまんでいるかどうか
+        self.pinch_flag = False
+        self.matchstick = init_box 
+        self.matchstick_points = [None] * N
+        #前の座標
+        self.prepoint = None
+    def hand(self, img, landmarks):
+        image_width, image_height = img.shape[1], img.shape[0]
+        landmark_point = []
+        for _, landmark in enumerate(landmarks.landmark):
+            if landmark.visibility < 0 or landmark.presence < 0:
+                continue
+
+            # 画面上の座標位置へ変換
+            landmark_x = min(int(landmark.x * image_width), image_width - 1)
+            landmark_y = min(int(landmark.y * image_height), image_height - 1)
+            landmark_z = landmark.z
+
+            landmark_point.append([landmark_x, landmark_y, landmark_z])
+        for i in range(len(landmark_point)):
+        #サークルを描画
+            cv2.circle(img, (landmark_point[i][0], landmark_point[i][1]), 7, (0, 0, 255), -1)
+    
+    def combi(self, img):
+        bimg  = cv2.imread('./imgs/matchstick.jpg')
+        replay_img  = cv2.imread('./imgs/replay.png')
+        # replay_img = cv2.cvtColor(replay_img,cv2.COLOR_BGR2RGB)
+        bimg = cv2.resize(bimg, dsize=None, fx=0.2, fy=0.2)
+        # bimg = cv2.cvtColor(bimg,cv2.COLOR_BGR2RGB)
+        white = np.ones((img.shape), dtype=np.uint8) * 255 #カメラ画像と同じサイズの白画像
+        white[0:replay_img.shape[0],img.shape[1]-replay_img.shape[1]:img.shape[1]] = replay_img
+        
+        #数字の配置
+        x = 20
+        y = 150
+        for i in range(3):
+            for j in range(2):
+                #マッチ棒の向きの変更
+                mimg = np.rot90(bimg,j)
+                xd = mimg.shape[1]
+                yd = mimg.shape[0]
+                #マッチ棒が縦向きの場合  
+                #左上から時計回り (0~N) 
+                if j == 0:
+                    if self.matchstick[i*7] != 0:
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7] = [x,y,xd+x,y+yd]
+                    x += yd
+                    if self.matchstick[i*7+1] != 0:
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+1] = [x,y,xd+x,y+yd]
+                    y += yd
+                    if self.matchstick[i*7+2] != 0:
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+2] = [x,y,xd+x,y+yd]
+                    x -= yd
+                    if self.matchstick[i*7+3] != 0:
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+3] = [x,y,xd+x,y+yd]
+                    y -= yd
+                #マッチ棒が横向きの場合
+                #上から順に
+                if j==1:
+                    if self.matchstick[i*7+4] != 0:
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+4] = [x,y,xd+x,y+yd]
+                    y += xd
+                    if self.matchstick[i*7+5] != 0: 
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+5] = [x,y,xd+x,y+yd]
+                    y += xd
+                    if self.matchstick[i*7+6] != 0: 
+                        white[y:yd+y,x:xd+x] = mimg
+                    self.matchstick_points[i*7+6] = [x,y,xd+x,y+yd]
+                    y -= 2*xd
+            mimg = bimg
+            x += mimg.shape[0] + 135
+
+        #記号の配置
+        #固定 (- , =)
+        #(-)の配置
+        mimg = np.rot90(bimg,1)
+        xd = mimg.shape[1]
+        yd = mimg.shape[0] 
+        x,y = 40+bimg.shape[0], 150+bimg.shape[0]
+        white[y:yd+y,x:xd+x] = mimg
+
+        #(=)の配置
+        x += x+bimg.shape[0]
+        y -= 20
+        white[y:yd+y,x:xd+x] = mimg
+        y += 40
+        white[y:yd+y,x:xd+x] = mimg
+        
+        #(|)の配置
+        mimg = bimg
+        xd = mimg.shape[1]
+        yd = mimg.shape[0] 
+        x, y = 80+bimg.shape[0], 150+(bimg.shape[0]//2)
+        if self.matchstick[21] != 0: 
+            white[y:yd+y,x:xd+x] = mimg
+        self.matchstick_points[21] = [x,y,xd+x,y+yd]
+        dwhite = white
+        img[dwhite!=[255, 255, 255]] = dwhite[dwhite!=[255, 255, 255]]
+        return img
+    
+    def pinch(self, img, point):
+        #つまんだ座標
+        points = [(point[0][0]+point[1][0])//2,(point[0][1]+point[1][1])//2]
+        #つまんでいると判断される場合
+        if abs(point[0][0]-point[1][0])<=15 and abs(point[0][1]-point[1][1])<=25:
+            cv2.circle(img, (points[0], points[1]), 7, (0, 255, 255), 3)
+            
+            #マッチ棒があるか
+            #matchstick_point[x_start,y_start,x_end,y_end]
+            for i, matchstick_point in enumerate(self.matchstick_points):
+                if self.moving == False and matchstick_point[0] <= points[0] <= matchstick_point[2]:
+                    if matchstick_point[1] <= points[1] <= matchstick_point[3]:
+                        if self.matchstick[i] != 0:
+                            self.matchstick[i] = 0
+                            # print(i)
+                            self.moving = True
+                            self.pinch_flag = True
+        #マッチ棒をとり、指を離した場合
+        elif self.moving == True:
+            for i, matchstick_point in enumerate(self.matchstick_points):
+                if matchstick_point[0] <= self.prepoint[0] <= matchstick_point[2]:
+                    if matchstick_point[1] <= self.prepoint[1] <= matchstick_point[3]:
+                        if self.matchstick[i] != 1:
+                            self.matchstick[i] = 1
+                            # print(i)
+                            self.moving = False
+                            self.pinch_flag = False
+        self.prepoint = points    
+        return self.pinch_flag
+    
+    def correct(self):
+        flag = False
+        correct_box = (1,1,1,0,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1,1,1,0)
+        if tuple(self.matchstick)==correct_box:
+            flag = True
+        return flag
+    
+    def restart(self):
+        self.__init__()
+        
+    def move(self, img, landmarks):
+        image_width, image_height = img.shape[1], img.shape[0]
+        landmark_point = []
+        mimg  = cv2.imread('./imgs/matchstick.jpg')
+        mimg = cv2.resize(mimg, dsize=(12,100))
+        white = np.ones((img.shape), dtype=np.uint8) * 255
+
+        for _, landmark in enumerate(landmarks.landmark):
+            if landmark.visibility < 0 or landmark.presence < 0:
+                continue
+
+            # 画面上の座標位置へ変換
+            landmark_x = min(int(landmark.x * image_width), image_width - 1)
+            landmark_y = min(int(landmark.y * image_height), image_height - 1)
+            landmark_z = landmark.z
+
+            landmark_point.append([landmark_x, landmark_y, landmark_z])
+
+        x = mimg.shape[1]//2
+        y = mimg.shape[0]//2
+        point = [landmark_point[4],landmark_point[8]]
+        flag = self.pinch(img,point)
+        #つかんでいるマッチ棒の表示
+        if flag:
+            if landmark_point[8][1] >= y and landmark_point[8][1]<=img.shape[0]-y:
+                if landmark_point[8][0] >= x and landmark_point[8][0]<=img.shape[1]-x:
+                    white[landmark_point[8][1]-y:landmark_point[8][1]+y,landmark_point[8][0]-x:landmark_point[8][0]+x] = mimg
+                    dwhite = white
+                    img[dwhite!=[255, 255, 255]] = dwhite[dwhite!=[255, 255, 255]]
+
+        #正解に合わせてメッセージを表示
+        if self.correct():
+            cv2.putText(img, "Great!", (20,80), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,0,0), 2)
+        # 64 x 64の画像を使用    
+        if landmark_point[8][1] >= 0 and landmark_point[8][1]<=64:
+                if landmark_point[8][0] >= img.shape[1]-64 and landmark_point[8][0]<=img.shape[1]:
+                    self.restart()
+                    cv2.putText(img, "Restart!", (20,60), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255), 2)
+        return img
